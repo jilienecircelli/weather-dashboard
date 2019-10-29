fbb75e0cb474b651089e606d42ad1ec2cfef6dc0
Jiliene Circelli <jilienecircelli@gmail.com>
2019-10-28 21:36:02 -0700

    added icons for forecast

--- script.js
+++ script.js
@@ -27,7 +27,6 @@ $("button").on("click", function() {
     renderCities()
 
 
-
     // // var str = JSON.stringify(city);
     // // var obj = JSON.parse(str)
     // $(".search-data").append(city + "<br>");
@@ -80,8 +79,21 @@ $("button").on("click", function() {
         console.log(responseTwo);
 
         // Add the icon based on the forecasted weather
-
-        var icon = responseTwo.list
+        // Day 1
+        var icon1 = responseTwo.list[4].weather[0].icon;
+        var icon1url = "http://openweathermap.org/img/w/" + icon1 + ".png";
+        // Day 2
+        var icon2 = responseTwo.list[12].weather[0].icon;
+        var icon2url = "http://openweathermap.org/img/w/" + icon2 + ".png";
+        // Day 3
+        var icon3 = responseTwo.list[20].weather[0].icon;
+        var icon3url = "http://openweathermap.org/img/w/" + icon3 + ".png";
+        // Day 4
+        var icon4 = responseTwo.list[28].weather[0].icon;
+        var icon4url = "http://openweathermap.org/img/w/" + icon4 + ".png";
+        // Day 5
+        var icon5 = responseTwo.list[36].weather[0].icon;
+        var icon5url = "http://openweathermap.org/img/w/" + icon5 + ".png";
 
         // Converts the temp to Kelvin with the below formula & then sets it to 2 decimal points
         var tempOneF = (responseTwo.list[4].main.temp - 273.15) * 1.80 + 32;
@@ -108,23 +120,27 @@ $("button").on("click", function() {
 
 
         $("#day-1").html("<h6>" + dayone.substr(0, 10) + "</h6>")
-        $("#day-1").append()
+        $("#day-1").append("<img src=" + icon1url + ">")
         $("#day-1").append("<p>" + "Temp: " + tempOne + "</p>")
         $("#day-1").append("<p>" + "Humidity: " + responseTwo.list[4].main.humidity + "</p>")
 
         $("#day-2").html("<h6>" + daytwo.substr(0, 10) + "</h6>")
+        $("#day-2").append("<img src=" + icon2url + ">")
         $("#day-2").append("<p>" + "Temp: " + tempTwo + "</p>")
         $("#day-2").append("<p>" + "Humidity: " + responseTwo.list[12].main.humidity + "</p>")
 
         $("#day-3").html("<h6>" + daythree.substr(0, 10) + "</h6>")
+        $("#day-3").append("<img src=" + icon3url + ">")
         $("#day-3").append("<p>" + "Temp: " + tempThree + "</p>")
         $("#day-3").append("<p>" + "Humidity: " + responseTwo.list[20].main.humidity + "</p>")
 
         $("#day-4").html("<h6>" + dayfour.substr(0, 10) + "</h6>")
+        $("#day-4").append("<img src=" + icon4url + ">")
         $("#day-4").append("<p>" + "Temp: " + tempFour + "</p>")
         $("#day-4").append("<p>" + "Humidity: " + responseTwo.list[28].main.humidity + "</p>")
 
         $("#day-5").html("<h6>" + dayfive.substr(0, 10) + "</h6>")
+        $("#day-5").append("<img src=" + icon5url + ">")
         $("#day-5").append("<p>" + "Temp: " + tempFive + "</p>")
         $("#day-5").append("<p>" + "Humidity: " + responseTwo.list[36].main.humidity + "</p>")
 
